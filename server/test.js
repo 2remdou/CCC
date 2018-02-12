be56ffe82a6d7c52b37f06edb33cad190bfebfc6
2remdou <2remdou@gmail.com>
2018-02-12 05:59:10 +0000

    modif gitignore

--- server/test.js
+++ server/test.js
@@ -4,6 +4,7 @@ const client = new Client();
 (
     async () => {
         await client.connect();
+        console.log('toure');
 
         const res = await client.query('SELECT $1::text as message', ['Hello world!']);
         console.log(res.rows[0].message); // Hello world!
